8cac4e67cdb5d2e6def722e18c4dbbd7ad265b37
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-13 11:26:06 +0700

    challenge 2 solution

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 // function addAll() {
 // 	var args = Array.prototype.slice.call(arguments);
 // 	var sum = 0;
-// 	for (i = 0; i < args.length; i++) {
+// 	for (let i = 0; i < args.length; i++) {
 // 		sum += args[i];
 // 	}
 // 	return sum;
@@ -29,7 +29,36 @@
 /////////////
 // solution 3  using ...rest and reduce
 
-function addAll(...numbers) {
-	return numbers.reduce((sum, num) => sum + num);
+// function addAll(...numbers) {
+// 	return numbers.reduce((sum, num) => sum + num);
+// }
+// console.log(addAll(2, 5, 5, 6, 7));
+
+//////////////////////////////////////////////
+
+/// CHALLENGE 2 SUM ALL PRIMES
+// pass in a number to loop up to and add all the prime numbers.
+// A prime number is a whole number greater than 1 whoes only factors are 1 and itself.
+/// EX   sumAllPrime(10) == 17
+
+function sumAllPrime(num) {
+	let sum = 0;
+
+	function checkForPrime(i) {
+		for (let x = 2; x < i; x++) {
+			if (i % x === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	for (let i = 2; i <= num; i++) {
+		if (checkForPrime(i)) {
+			sum += i;
+		}
+	}
+	return sum;
 }
-console.log(addAll(2, 5, 5, 6, 7));
+
+console.log(sumAllPrime(10));
