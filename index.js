242fcfb5f0009ac871f819b72ad14711bd233d29
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-13 10:15:00 +0700

    challenge 1 solution 2

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-// CHALLENGE 1  : ADD ALL NUMBERS
-// RETURN A SUM OF ALL PARAMETERS ENTERED REGARDLES OF THE AMOUNT OF NUMBERS
-// NO ARRAYS .... EXAMPLE  addAll(2, 5, 6, 7) === 20
+/// CHALLENGE 1  : ADD ALL NUMBERS
+/// RETURN A SUM OF ALL PARAMETERS ENTERED REGARDLES OF THE AMOUNT OF NUMBERS
+/// NO ARRAYS .... EXAMPLE  addAll(2, 5, 6, 7) === 20
 
-// solution 1 using ES5 arguments object (an array like object) and for loop
+/// solution 1 using ES5 arguments object (an array like object) and for loop
 
-function addAll() {
-	var args = Array.prototype.slice.call(arguments);
-	var sum = 0;
-	for (i = 0; i < args.length; i++) {
-		sum += args[i];
-	}
+// function addAll() {
+// 	var args = Array.prototype.slice.call(arguments);
+// 	var sum = 0;
+// 	for (i = 0; i < args.length; i++) {
+// 		sum += args[i];
+// 	}
+// 	return sum;
+// }
+
+// console.log(addAll(2, 5, 5, 6, 7));
+//////////////
+
+//// solution 2
+///  using  ...rest and forEach loop.
+function addAll(...numbers) {
+	let sum = 0;
+	numbers.forEach((num) => (sum += num));
 	return sum;
 }
 
